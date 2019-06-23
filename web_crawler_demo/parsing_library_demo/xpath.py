13098a9e185881dc0ec3c25df62320e002856c1f
wanglin <newiflin@gmail.com>
2019-06-23 15:16:15 +0800

    lxml(xpath解析)

--- web_crawler_demo/parsing_library_demo/xpath.py
+++ web_crawler_demo/parsing_library_demo/xpath.py
@@ -31,4 +31,75 @@ print('\n3.子节点')
 result = html.xpath('//li/a')
 print(result)
 
+print('\n4.父节点')
+result = html.xpath('//a[@href="link4.html"]/../@class')
+print(result)
+result = html.xpath('//a[@href="link4.html"]/parent::*/@class')
+print(result)
 
+print('\n5.属性匹配')
+result = html.xpath('//li[@class="item-0"]')
+print(result)
+
+print('\n6.文本获取')
+result = html.xpath('//li[@class="item-0"]//text()')    #获取所有子孙结点文本
+print(result)
+result = html.xpath('//li[@class="item-0"]/a/text()')   #获取子节点a标签的文本
+print(result)
+
+print('\n7.属性获取')
+result = html.xpath('//li/a/@href')
+print(result)
+
+print('\n8.属性多值匹配')
+text = '''
+<li class="li li-class"><a href="index.html">first-item</a></li>
+'''
+html = etree.HTML(text)
+result = html.xpath('//li[contains(@class, "li")]/a/text()')
+print(result)
+
+print('\n9.多属性匹配')
+text = '''
+<li class="li li-class" name="item"><a href="link-html">first item</a></li>
+'''
+html = etree.HTML(text)
+result = html.xpath('//li[contains(@class, "li") and @name="item"]/a/text()')
+print(result)
+
+print('\n10.按序选择')
+html = etree.parse('test.html', etree.HTMLParser())
+result = html.xpath('//li[1]/a/text()')     #选择第一个li结点的直接子节点a的文本
+print(result)
+result = html.xpath('//li[last()]/a/text()')    #选择倒数第一个...
+print(result)
+result = html.xpath('//li[position()<3]/a/text()') #选择位置小于3...
+print(result)
+result = html.xpath('//li[last()-2]/a/text()') #选择倒数第三个...
+print(result)
+
+print('\n11.节点轴选择')
+text = '''
+<div>
+<ul>
+<li class="item-0"><a href="link1.html"><span>first item</span></a></li>
+<li class="item-1"><a href="link2.html"><span>second item</span></a></li>
+<li class="item-inactive"><a href="link3.html"><span>third item</span></a></li>
+<li class="item-0"><a href="link4.html"><span>fourth item</span></a></li>
+<li class="item-1"><a href="link5.html"><span>fifth item</span></a>
+'''
+html = etree.HTML(text)
+result = html.xpath('//li[1]/ancestor::*')
+print(result)
+result = html.xpath('//li[1]/ancestor::div')
+print(result)
+result = html.xpath('//li[1]/attribute::*')
+print(result)
+result = html.xpath('//li[1]/child::a[@href="link1.html"]')
+print(result)
+result = html.xpath('//li[1]/descendant::span')
+print(result)
+result = html.xpath('//li[1]/following::*[2]')
+print(result)
+result = html.xpath('//li[1]/following-sibling::*')
+print(result)
