bc4798c9aeea0c3ddc8e7c910180f10fa64380ab
wanglin <newiflin@gmail.com>
2019-07-04 15:56:32 +0800

    解析库 pyquery

--- web_crawler_demo/parsing_library_demo/Pyquery.py
+++ web_crawler_demo/parsing_library_demo/Pyquery.py
@@ -126,7 +126,7 @@ print(li)
 print('remove')
 html = '''
 <div class="wrap">
-    Hello, World
+    Hello, Worldb
 <p>This is a paragraph</p>
 </div>
 '''
@@ -135,3 +135,31 @@ wrap = doc('.wrap')
 print(wrap.text())
 wrap.find('p').remove()
 print(wrap.text())
+
+print('7.伪类选择器')
+html = '''
+<div class="wrap">
+<div id="container">
+<ul class="list">
+<li class="item-0">first item</li>
+<li class="item-1"><a href="link2.html">second item</a></li>
+<li class="item-0 active"><a href="link3.html"><span class="bold">third item</span></a></li>
+<li class="item-1 active"><a href="link4.html">fourth item</a></li>
+<li class="item-0"><a href="link5.html">fifth item</a></li>
+</ul>
+</div>
+</div>
+'''
+doc = pq(html)
+li = doc('li:first-child')
+print(li)
+li = doc('li:last-child')
+print(li)
+li = doc('li:nth-child(2)')
+print(li)
+li = doc('li:gt(2)')    #第三个li节点之后的节点（这里就是第4和第5个li节点）
+print(li)
+li = doc('li:nth-child(2n)')
+print(li)
+li = doc('li:contains(second)')
+print(li)
